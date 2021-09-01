01bec8534fbd0cb907b245133eb80f91bf8992ae
Anders Refsdal Olsen <andersro93@ros-nett.com>
2021-09-01 08:40:52 +0200

    Added front page description

--- src/Frontpage/FrontPage.tsx
+++ src/Frontpage/FrontPage.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Typography from "@material-ui/core/Typography";
 
 const FrontPage: React.FC = () => {
 
   return (
-    <h1>Frontpage</h1>
+      <>
+        <Typography variant="h3">
+          Welcome to Voterr
+        </Typography>
+        <Typography variant="body1">
+          This is a demo application that is used to demonstrate how modern authentication works using MSAL, Dotnet and React.
+          You might not be able to sign in to this application if MPN is not configured in Azure or you are not an admin that may grant access to this application to your Azure AD.
+        </Typography>
+      </>
+
   );
 };
 
